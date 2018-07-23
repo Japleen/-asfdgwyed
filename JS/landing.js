454d3658582112fd43edcfbad8355dc102b5c692
Japleen <japleen.kaur@wipro.com>
2018-07-23 11:57:11 +0530

    POST request

---
+++ JS/landing.js
@@ -0,0 +1,56 @@
+function postRequest (){
+    var name=document.getElementById("name").value;
+    var email=document.getElementById("email").value;
+    
+    var data={};
+    data.email=email;
+    data.name=name;
+    console.log(data)
+    
+    var message;
+    var http = new XMLHttpRequest();
+    var url = 'https://jsonplaceholder.typicode.com/posts';  // Server URL
+    //var params = 'orem=ipsum&name=binny';
+    http.open('POST', url, true);
+    
+    //Send the proper header information along with the request
+    http.setRequestHeader('Content-type', 'application/json');
+    
+    console.log(http.responseText)
+    http.onreadystatechange = function() {//Call a function when the state changes.
+        if(http.readyState == 4 && http.status == 200) {
+            alert(http.responseText);
+            message="Thank you for subscribing to Sonderblu newsletter, we will keep you in touch!";
+        }
+    }
+    http.send(data);
+    
+    
+    }
+
+    function displayPopup(){
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+    }
