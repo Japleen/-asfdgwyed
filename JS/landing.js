ae5896df9bae40a135821e950581c3445694e8d5
anshul <anshul.nautiyal1@wipro.com>
2018-07-26 23:15:31 +0530

    Make website button and video responsive

--- JS/landing.js
+++ JS/landing.js
@@ -1,21 +1,21 @@
 function postRequest (){
     var name=document.getElementById("name").value;
     var email=document.getElementById("email").value;
-    
+
     var data={};
     data.email=email;
     data.name=name;
     //console.log(data)
-    
+
     var message;
     var http = new XMLHttpRequest();
     var url = 'https://jsonplaceholder.typicode.com/posts';        // Server URL
     //var params = 'orem=ipsum&name=binny';
     http.open('POST', url, true);
-    
+
     //Send the proper header information along with the request
     http.setRequestHeader('Content-type', 'application/json');
-    
+
    // console.log(http.responseText)
     http.onreadystatechange = function() {//Call a function when the state changes.
         if(http.readyState == 4 && http.status == 200) {
@@ -27,7 +27,7 @@ function postRequest (){
             displayPopup(message);
 
         }
-        else if(http.readyState == 4 && http.status !== 500) { 
+        else if(http.readyState == 4 && http.status !== 500) {
             message="We are experiencing temporary technical difficulties, please try again later";
             displayPopup(message);
 
@@ -38,7 +38,7 @@ function postRequest (){
     }
  }
 
-    
+
 
 function displayPopup(message){
       //  console.log("here")
@@ -50,7 +50,7 @@ var modal = document.getElementById('myModal');
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
+// When the user clicks the button, open the modal
 
     modal.style.display = "block";
       //  $(#mod)
@@ -59,7 +59,7 @@ var span = document.getElementsByClassName("close")[0];
 
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+i.onclick = function() {
     modal.style.display = "none";
 }
 
@@ -76,6 +76,7 @@ $(document).ready(function(e){
     $("#emailForm").hide();
 
     $('#emailButton').click(function(){
+      alert();
         $("#emailForm").show();
 
     });
